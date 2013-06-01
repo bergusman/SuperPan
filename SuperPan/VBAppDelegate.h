b0827927bdd67d97296bffb4481e8e2a07cb01be
Vitaliy Berg <bergusman@gmail.com>
2013-06-01 14:23:46 +0400

    Initial Commit

---
+++ SuperPan/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  SuperPan
+//
+//  Created by Vitaliy Berg on 6/1/13.
+//  Copyright (c) 2013 Vitaliy Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SuperPan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SuperPan
+//
+//  Created by Vitaliy Berg on 6/1/13.
+//  Copyright (c) 2013 Vitaliy Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VBAppDelegate class]));
+    }
+}
